7b89580a2176b7cb3b92e04a77018aacc0a9acc8
T-zemmari <76817211+T-zemmari@users.noreply.github.com>
2021-02-18 19:58:10 +0100

    connection to the port achieved

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const Port = 3000;
 
 
-app.listen('Escuchando en el puesto:',3000);
+
+
+
+
+app.listen(Port, ()=>console.log(`Escuchando en el puerto:${Port}`));
+console.log("Estoy Aqui y ahora")
