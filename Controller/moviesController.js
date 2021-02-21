2ae2182a8b851d3af1ad392bb648295ee06d9d32
T-zemmari <76817211+T-zemmari@users.noreply.github.com>
2021-02-21 13:45:18 +0100

    a estas alturas por un error grave de mongoose que no me reconocia la instancia del modelo cuando hacia el get aunque si en el post, he tenido que modificar casi todo mi codigo hasta ahora.

--- Controller/moviesController.js
+++ Controller/moviesController.js
@@ -1,8 +1,46 @@
-const moviesDb= require('./moviesController');
+
+const mongoose = require('mongoose');
+const movie = require('../models/moviesDb');
 
 
+class Film {
 
+    constructor(){
 
+    }
 
+    //GET - Return all Films in the DB
 
-  
+    async findAllFilms(filmCollection){
+        return movie.find(filmCollection);
+    };
+
+    //GET - Return a Film with specified ID
+
+    async findById({id: id}) {
+        return movie.findOne({id: id});
+    };
+
+    //POST - Insert a new Film in the DB
+
+    async addFilm(film){
+       return movie.create(film)
+    };
+
+    //PUT - Update a register already exists
+
+    async updateFilm(id, film){
+        const idFound = movie.findOne({id: id})
+        return idFound.update(film)
+    };
+
+    //DELETE - Delete a TVShow with specified ID
+
+    async deleteFilm({id: id}) {
+        const idFound = movie.findOne({id: id})
+        return idFound.remove()
+    };
+};
+
+let movieController = new Film();
+module.exports = movieController;
